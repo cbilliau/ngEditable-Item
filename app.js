53051d14ae3b3670433b22f0fb0249b95e75e71f
cbilliau <christopher@billiau.com>
2016-08-31 13:35:12 -0400

    I am confused by instructions but I think that the directive 'makeEditable' is supposed to insert the makeEditable-template.html which would contatin the edit and save buttons described in the instructions. So far the template is overwriting the <p> in the div.

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+'esversion: 6';
+
 var app = angular.module('makeEditableApp', []);
 
 app.directive('makeEditable', function() {
   return {
-    restrict: 'E',
+    restrict: 'A',
+    transclude: true,
     templateUrl: 'makeEditable-template.html',
-    replace: true
-  };
-});
+    replace: false,
+    scope: true,
+    link: function(scope, element, attrs) {
+      let textToEdit = element.find('p')[0];
+      scope.change = {
+        edit: function() {
+          console.log(textToEdit);
+        }
+      };
+
+      }
+    };
+  });
